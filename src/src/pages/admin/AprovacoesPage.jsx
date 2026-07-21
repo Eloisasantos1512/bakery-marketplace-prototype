c2242caabb2ab23100db1fadeaa9fa401193456c
Eloisasantos1512 <eloisa.santos@scitec.com.br>
2026-07-21 17:50:11 +0000

    ok

--- src/src/pages/admin/AprovacoesPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AdminApprovalDashboard from "../../components/AdminApprovalDashboard";
-
-export default function AprovacoesPage() {
-  return <AdminApprovalDashboard />;
-}
--- src/src/pages/customer/OrdersPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import CustomerOrderTracker from "../../components/CustomerOrderTracker";
-
-export default function OrdersPage() {
-  return <CustomerOrderTracker approvalStatus="approved" />;
-}
